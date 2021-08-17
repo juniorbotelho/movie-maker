b6ee6f5cce4cf79cfc1c488aa9fa26be3eff23f8
Junior Botelho <junowerther@gmail.com>
2021-08-17 13:39:12 -0300

    fix: solve the 'cli' binary problem where the tsconfig file path was incorrect

--- cli/src/cli.ts
+++ cli/src/cli.ts
@@ -3,7 +3,7 @@ import { build } from 'gluegun'
 /**
  * Create the cli and kick it off
  */
-async function run(argv) {
+export async function run(argv) {
   // create a CLI runtime
   const cli = build()
     .brand('cli')
