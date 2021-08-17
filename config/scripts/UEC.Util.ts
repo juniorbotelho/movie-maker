e49f6e605de7e6d732b5ebb32fbdb451f42d868a
Junior Botelho <junowerther@gmail.com>
2021-08-16 21:09:58 -0300

    style: add style and pattern libraries to the project

--- config/scripts/UEC.Util.ts
+++ config/scripts/UEC.Util.ts
@@ -4,21 +4,20 @@ declare type UECUtilConfig = typeof config
 declare type UECUtilCallback = (opt: DotenvConfigOutput) => void
 
 export class UECUtil {
-
   private options: DotenvConfigOptions = null
   private config: UECUtilConfig = null
 
-  public setLoader(config: UECUtilConfig) {
+  public setLoader(config: UECUtilConfig): UECUtil {
     this.config = config
     return this
   }
 
-  public setOption(options?: DotenvConfigOptions) {
+  public setOption(options?: DotenvConfigOptions): UECUtil {
     this.options = options
     return this
   }
 
-  public builder(callback?: UECUtilCallback) {
+  public builder(callback?: UECUtilCallback): void {
     const confObj = this.config(this.options)
     return callback(confObj)
   }
