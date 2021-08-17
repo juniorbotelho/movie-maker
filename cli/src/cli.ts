3423befd8aa1da80af1f75e288029fb3643c26e0
Junior Botelho <junowerther@gmail.com>
2021-08-17 13:48:38 -0300

    fix: specify the file to eslint.ignore and set the typings to cli.ts

--- cli/src/cli.ts
+++ cli/src/cli.ts
@@ -1,25 +1,29 @@
-import { build } from 'gluegun'
+import { GluegunToolbox, build } from 'gluegun'
+import { Options } from 'gluegun/build/types/domain/options'
+
+declare type CLIToolbox = (
+  rawCommand?: string | Options,
+  extraOptions?: Options
+) => Promise<GluegunToolbox>
 
 /**
- * Create the cli and kick it off
+ * Create an initial gluegun command
+ * line executable, the cli and kick it off
  */
-export async function run(argv) {
-  // create a CLI runtime
+export async function run(argv: CLIToolbox): Promise<GluegunToolbox> {
   const cli = build()
     .brand('cli')
     .src(__dirname)
-    .plugins('./node_modules', { matching: 'cli-*', hidden: true })
-    .help() // provides default for help, h, --help, -h
-    .version() // provides default for version, v, --version, -v
+    .plugins('../node_modules', {
+      matching: 'videomaker-*',
+      hidden: true,
+      required: false,
+    })
+    .help()
+    .version()
     .create()
-  // enable the following method if you'd like to skip loading one of these core extensions
-  // this can improve performance if they're not necessary for your project:
-  // .exclude(['meta', 'strings', 'print', 'filesystem', 'semver', 'system', 'prompt', 'http', 'template', 'patching', 'package-manager'])
-  // and run it
+
   const toolbox = await cli.run(argv)
 
-  // send it back (for testing, mostly)
   return toolbox
 }
-
-export default { run }
