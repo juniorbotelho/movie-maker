d07441a240761c37f5b97e970e6a55d08ba05044
Junior Botelho [DO] <debotelho@yahoo.com>
2021-10-07 14:41:28 -0300

    feat: add slideshow support for powerpoint api

--- service/SlideShow.ts
+++ service/SlideShow.ts
@@ -6,6 +6,12 @@ const Service = () => ({
     Main.Application(async ({ ctx, application }) => {
       const content = application.state.load()
 
+      const transaction = ctx.sentry.startTransaction({
+        name: 'Slideshow',
+        op: 'Service/Slideshow',
+        description: 'Service to handle with slideshow as powerpoint module.',
+      })
+
       /**
        * Add user metadata to powerpoint project,
        * this data may or may not be personal.
@@ -23,12 +29,73 @@ const Service = () => ({
        * instagram slides.
        */
       application.powerpoint.defineLayout({
-        name: 'Instagram',
-        width: 50.8,
-        height: 50.8,
+        name: 'INSTAGRAM',
+        width: 11.25,
+        height: 11.25,
       })
-      application.powerpoint.layout = 'Instagram'
-      application.powerpoint.rtlMode = true
+      application.powerpoint.layout = 'INSTAGRAM'
+      application.powerpoint.rtlMode = false
+
+      try {
+        application.powerpoint.defineSlideMaster({
+          title: 'MASTER_SLIDE',
+          margin: [0.25, 0.25, 0.25, 0.25],
+          background: {
+            path: 'temp/texture.jpg',
+            transparency: 20,
+          },
+          objects: [
+            {
+              image: {
+                path: 'temp/background.jpg',
+                transparency: 40,
+                w: '100%',
+                h: '100%',
+              },
+            },
+          ],
+          slideNumber: {
+            fontSize: 23.6,
+            align: 'right',
+            color: 'ffffff',
+            w: '100%',
+            h: 0.52,
+            y: 10.54,
+          },
+        })
+
+        const slide = application.powerpoint.addSlide({
+          masterName: 'MASTER_SLIDE',
+        })
+
+        slide.addShape('squareTabs', {
+          align: 'center',
+          w: '100%',
+          h: '100%',
+        })
+
+        slide.addText('Hello World!', {
+          fontFace: 'Abadi',
+          fontSize: 32,
+          align: 'center',
+          x: 0,
+          y: 0.52,
+          w: '100%',
+          h: '10%',
+        })
+
+        await application.powerpoint.writeFile({
+          fileName: 'temp/presentation.pptx',
+          compression: false,
+        })
+
+        process.exit()
+      } catch (error) {
+        ctx.logger.error(error)
+        ctx.sentry.captureException(error)
+      } finally {
+        transaction.finish()
+      }
     }),
 })
 
@@ -37,3 +104,5 @@ const Service = () => ({
  * as the basis for all videoshow functions.
  */
 export const Context = Service()
+
+Context.slideshow()
