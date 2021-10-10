ff0c7acd3dc619dc12dc4b6ca8eec635366a288b
Junior Botelho [DO] <debotelho@yahoo.com>
2021-10-10 00:35:18 -0300

    feat: add script to run server

--- server/Http.ts
+++ server/Http.ts
@@ -5,8 +5,6 @@ import * as Http from 'http'
 Main.Application(async ({ ctx }) => {
   const express = Express()
 
-  express.use(Express.static('public'))
-
   const server = Http.createServer(express)
 
   server.listen(3000, () => ctx.logger.info('🚀 Loaded: Server'))
