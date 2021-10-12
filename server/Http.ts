054323e90dc413d4a703a021edd2020bebc1734d
Junior Botelho [DO] <debotelho@yahoo.com>
2021-10-12 01:19:36 -0300

    refact: refact public assets and server index

--- server/Http.ts
+++ server/Http.ts
@@ -5,6 +5,8 @@ import * as Http from 'http'
 Main.Application(async ({ ctx }) => {
   const express = Express()
 
+  express.use(Express.static('public'))
+
   const server = Http.createServer(express)
 
   server.listen(3000, () => ctx.logger.info('🚀 Loaded: Server'))
